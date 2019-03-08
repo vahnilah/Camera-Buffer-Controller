dd05fb50708b21d162709968b3ca0804bc0c1856
vahnilah <ssjgokuy2k@gmail.com>
2019-03-07 17:39:18 -0800

    Initial version
    Initial version to work with 2d style games

---
+++ Assets/CameraBufferDrag/CameraBufferDrag.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Attach a camera & player object to this class and then assign the correct joystick code used for your project.
+ * 
+ * This allow the camera to create a buffer of movement to pan the camera on the joystick direction
+ * The decay rates can be adjusted the speed of the camera when moving back to center on the player
+ */
+[ExecuteInEditMode]
+public class CameraBufferDrag : MonoBehaviour
+{
+    public GameObject thisCamera;
+    public GameObject thisPlayer;
+    public string LeftJoystickXAxisCode = "LeftJoystickX";
+    public string LeftJoystickYAxisCode = "LeftJoystickY";
+
+    private float BufferX = 0f;
+    private float BufferY = 0f;
+
+    [Space(20)]
+    public bool isVerticalMovementOnZAxis = false;
+    private float X;
+    public float AdjustX = 0f;
+    private float Y;
+    public float AdjustY = 0f;
+    public float AdjustZoom = 0f;
+
+    [Space(20)]
+    public float BufferOfX = 10f;
+    public float BufferOfY = 10f;
+
+    [Space(20)]
+    public float decayRate = 2f;
+    public float ResistanceBufferOfX = 3f;
+    public float ResistanceBufferOfY = 3f;
+    public float StartingDecayBufferOfX = 50f;
+    public float EndingDecayBufferOfX = 11f;
+    public float StartingDecayBufferOfY = 50f;
+    public float EndingDecayBufferOfY = 11f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Change which axis vertical is on
+        if (isVerticalMovementOnZAxis == true)
+        {
+            var position = thisCamera.transform.position;
+            position.x = X + AdjustX;
+            position.z = Y + AdjustY;
+            position.y = AdjustZoom;
+            thisCamera.transform.position = position;
+        } else
+        {
+            var position = thisCamera.transform.position;
+            position.x = X + AdjustX;
+            position.y = Y + AdjustY;
+            position.z = AdjustZoom;
+            thisCamera.transform.position = position;
+        }
+
+        float playerPosX;
+        float playerPosY;
+
+        // Change which axis vertical is on
+        if (isVerticalMovementOnZAxis == true)
+        {
+            playerPosX = thisPlayer.transform.position.x;
+            playerPosY = thisPlayer.transform.position.z;
+        }
+        else
+        {
+            playerPosX = thisPlayer.transform.position.x;
+            playerPosY = thisPlayer.transform.position.y;
+        }
+
+        var cameraFollowsX = playerPosX;
+        var cameraFollowsY = playerPosY;
+
+        var cameraDragY = 0f;
+        var cameraDragX = 0f;
+
+        // Camera buffer
+        CameraBuffer();
+        cameraDragX += BufferX;
+        cameraDragY += BufferY;
+
+        X = cameraFollowsX + cameraDragX;
+        Y = cameraFollowsY + cameraDragY;
+    }
+
+    // Create a camera movement buffer for the direction of the player of the char
+    // This will auto adjust back to the player when the joystick is released
+    private void CameraBuffer()
+    {
+        // Add camera drag on X
+        var movementX = Input.GetAxis(LeftJoystickXAxisCode);
+        if (movementX > 0 && BufferX < BufferOfX)
+            BufferX += movementX / ResistanceBufferOfX;
+        if (movementX < 0 && BufferX > -BufferOfX)
+            BufferX += movementX / ResistanceBufferOfX;
+
+        // Add camera drag on Y
+        var movementY = Input.GetAxis(LeftJoystickYAxisCode);
+        if (movementY > 0 && BufferY < BufferOfY)
+            BufferY += movementY / ResistanceBufferOfY;
+        if (movementY < 0 && BufferY > -BufferOfY)
+            BufferY += movementY / ResistanceBufferOfY;
+
+        // Normalize 
+        var BufferXNorm = BufferX / StartingDecayBufferOfX;
+        var BufferYNorm = BufferY / StartingDecayBufferOfY;
+        
+        // Get c from a^2 + b^2 = c^2
+        var c = Mathf.Sqrt(BufferXNorm * BufferXNorm + BufferYNorm * BufferYNorm);
+
+        // Obtain the ratio to decay with
+        var xRatio = (BufferXNorm / c);
+        var yRatio = (BufferYNorm / c);
+
+        // Adjust the speed of decay at different points of the decay
+        var ratioBufferOfX = BufferX / BufferOfX;
+        var ratioBufferOfY = BufferY / BufferOfY;
+        float DecayBufferOfX = ratioBufferOfX * StartingDecayBufferOfX / EndingDecayBufferOfX;
+        float DecayBufferOfY = ratioBufferOfY * StartingDecayBufferOfY / EndingDecayBufferOfY;
+
+        // Renormalize X with the decay rate to self adjust camera at an even rate with Y
+        if (Mathf.Abs(movementX) < .05f)
+        {
+            var adj = Mathf.Abs(xRatio / decayRate * DecayBufferOfX);
+            if (Mathf.Abs(BufferX) < .20f)
+                BufferX = 0;
+            else if (BufferX > 0)
+                BufferX -= Time.deltaTime * adj;
+            else if (BufferX < 0)
+                BufferX += Time.deltaTime * adj;
+        }
+
+        // Renormalize Y with the decay rate to self adjust camera at an even rate with X
+        if (Mathf.Abs(movementY) < .05f)
+        {
+            var adj = Mathf.Abs(yRatio / decayRate * DecayBufferOfY);
+            if (Mathf.Abs(BufferY) < .20f)
+                BufferY = 0;
+            else if (BufferY > 0)
+                BufferY -= Time.deltaTime * adj;
+            else if (BufferY < 0)
+                BufferY += Time.deltaTime * adj;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+Camera Buffer Drag Controller
+==========
+
+This script will set the camera to follow a player with a buffer towards the direction on a controller to show more of the area in level.
+
+This is useful for 2D or platformer games where you will want the camera to show what is ahead of the player towards the direction of the joystick.
+Settings can be adjusted to add to the buffer fast or slow and then decay back to the player at a linear rate or a variable setting.
+
+![gif](https://i.imgur.com/oBcK4sG.gif)
+![gif](https://i.imgur.com/NiMsvBj.gif)
+![gif](https://i.imgur.com/dJ4pQtl.gif)
+
+Installation
+------------
+
+Download the github project and copy the CameraBufferDrag.cs file to your unity project.
+Then drag the script to an empty gameobject or controller & attach a camera & character to the script and set the joystick string controls which match your project.
